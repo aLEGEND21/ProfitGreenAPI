e9a56d8852242796402219cbac6e0d178586f002
aLEGEND21 <arnavmurthi@gmail.com>
2022-05-30 14:52:47 -0400

    Updated the code to use the SECRET_KEY

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
+from config import Config
+
 
 def create_app():
     # Create the app
     app = Flask(__name__)
+    app.secret_key = Config.SECRET_KEY
     
     # Register blueprints
     from application.api import api
